053224df4e2420367aa2ecb1d592d2b153116982
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-28 16:55:18 +0100

    Check for wall collisions

--- js/main.js
+++ js/main.js
@@ -2,14 +2,20 @@
 const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
+const ballDiameter = 20;
+const boardWidth = 550;
+const boardHeight = 300;
 
-const boardWidth = 450;
+let xDirection = 2;
+let yDirection = 2;
 
 const startUserPossition = [230, 10];
 let currentUserPossition = startUserPossition;
 
 const startBallPossition = [270, 40];
 let currentBallPossition = startBallPossition;
+
+let timerId;
 
 ///////--BLOCKS--///////
 
@@ -78,7 +84,7 @@ const moveUser = (ev) => {
       }
       break;
     case "ArrowRight":
-      if (currentUserPossition[0] < boardWidth) {
+      if (currentUserPossition[0] < boardWidth - blockWidth) {
         currentUserPossition[0] += 20;
         drawUser();
       }
@@ -105,8 +111,43 @@ grid.appendChild(ball);
 
 //move ball
 const moveBall = () => {
-  currentBallPossition[0] += 2;
-  currentBallPossition[1] += 2;
+  currentBallPossition[0] += xDirection;
+  currentBallPossition[1] += yDirection;
   drawBall();
+  checkForCollisions();
 };
-setInterval(moveBall, 30);
+timerId = setInterval(moveBall, 30);
+
+//check for collisions
+const checkForCollisions = () => {
+  //check for wall collisions
+  if (
+    currentBallPossition[0] >= boardWidth - ballDiameter ||
+    currentBallPossition[1] >= boardHeight - ballDiameter ||
+    currentBallPossition[0] <= 0
+  ) {
+    changeDirection();
+  }
+  //check for game over
+  if (currentBallPossition[1] <= 0) {
+    clearInterval(timerId);
+  }
+};
+const changeDirection = () => {
+  if (xDirection === 2 && yDirection === 2) {
+    yDirection = -2;
+    return;
+  }
+  if (xDirection === 2 && yDirection === -2) {
+    xDirection = -2;
+    return;
+  }
+  if (xDirection === -2 && yDirection === -2) {
+    yDirection = 2;
+    return;
+  }
+  if (xDirection === -2 && yDirection === 2) {
+    xDirection = 2;
+    return;
+  }
+};
