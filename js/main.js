364665b23e888afec47adb32605d1cc87c588052
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-28 15:11:37 +0100

    Create an array from the Block class to create every block with the function addBlock

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 "use strict";
+const grid = document.querySelector(".grid");
+const blockWidth = 100;
+const blockHeight = 20;
 
-const block = document.createElement("div");
-block.classList.add("block");
+//create block
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
+
+//all my blocks
+const blocks = [new Block(10, 270)];
+//draw all my block
+const addBlocks = () => {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.style.left = blocks[i].bottomLeft[0] + "px";
+    block.style.bottom = blocks[i].bottomLeft[1] + "px";
+    grid.appendChild(block);
+  }
+};
+
+addBlocks();
